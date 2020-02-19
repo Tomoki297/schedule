503dfb520f0611acba317779f5314150cddf3510
Tomoki297 <tomo.baske.2549@icloud.com>
2020-02-19 22:23:19 +0900

    Create user_file

--- config/routes.rb
+++ config/routes.rb
@@ -17,15 +17,10 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root 'home#top'
   get 'top' => "home#top"
-  get 'about' => "home#about"
-  get 'schedule' => "home#schedule"
   get 'month' => "home#month"
-  get 'day1' => "home#day1"
-  get 'day2' => "home#day2"
-  get 'day3' => "home#day3"
-  get 'day4' => "home#day4"
-  get 'day5' => "home#day5"
-  get 'day6' => "home#day6"
-  get 'day7' => "home#day7"
+  get 'new' => "admin_users#new"
+  get 'login' => "admin_users#login_form"
+  post 'login' => "admin_users#login"
+  post 'logout' => "admin_users#logout"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
